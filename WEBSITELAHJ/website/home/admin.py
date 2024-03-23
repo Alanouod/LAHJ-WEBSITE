ebb3d5a894426b3455b931cc9bfb992008627d68
Alanoud <alanoudalharbi09@gmail.com>
2024-03-24 01:28:17 +0300

    add photos in inspiration page by admin

--- WEBSITELAHJ/website/home/admin.py
+++ WEBSITELAHJ/website/home/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Homeowner, Professional, UserProfile,PreviousWork,Wishlist,Order,Quote,Message
+from .models import Homeowner, Professional, UserProfile,PreviousWork,Wishlist,Order,Quote,Message,ProjectImage
 
 
 admin.site.register(Homeowner)
 admin.site.register(Professional)
 admin.site.register(UserProfile)
 admin.site.register(PreviousWork)
+admin.site.register(ProjectImage)
 admin.site.register(Wishlist)
 admin.site.register(Order)
 admin.site.register(Message)

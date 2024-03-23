17c0cf94f47b4444b23a4c98448d77508b190852
Alanoud <alanoudalharbi09@gmail.com>
2024-03-24 00:48:14 +0300

    edit the setting and urls

--- WEBSITELAHJ/website/website/urls.py
+++ WEBSITELAHJ/website/website/urls.py
@@ -20,16 +20,21 @@ from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
+from home import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('', views.home, name=''),
     path('', include('home.urls')),
     
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-
+# Retrieve images from /media/
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += staticfiles_urlpatterns()
-
